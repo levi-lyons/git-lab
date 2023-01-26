0035bba27b08c29004742384e375aceca4a89655
Levi <ll790121@ohio.edu>
2023-01-25 19:51:13 -0500

    program update

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*              File: git-lab-program.cc
+*            Author: Levi Lyons
+*              Date: 01/25/2023
+*       Description: Lab 2 for Git tutorial
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main (int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}//main
